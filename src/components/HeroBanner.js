74b4ebc57af35cb04709fcd5931d1882b8f9393d
anarita_cavalheiro <anarita.cavalheiro@hotmail.com>
2020-11-23 18:41:47 +0000

    done-ish

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function heroBanner() {
+    return(
+        <div className="HeroBanner-div">
+        <h1>Say hello to ReactJS</h1>
+        <p className="heather-p">You will learn how to use 
+        the most popular frontend library,
+        and become a super Ninja developer.</p>
+        <button type="button" className="btn btn-secondary btn-lg">Awesome!</button>
+        </div>
+    )
+}
+
+export default heroBanner
