998aa6e32f881a07054042724649c58f6b88c017
anarita_cavalheiro <anarita.cavalheiro@hotmail.com>
2020-11-24 10:02:21 +0000

    css almost done

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -3,6 +3,9 @@ import React from 'react';
 function heroBanner() {
     return(
         <div className="HeroBanner-div">
+            <img className="ironhack-logo" src="/images/ironhack-log.svg"/>
+            <img className="menu" src="/images/menu-top.svg"/>
+            <img className="react-logo" src="/images/react-logo.svg"/>
         <h1>Say hello to ReactJS</h1>
         <p className="heather-p">You will learn how to use 
         the most popular frontend library,
